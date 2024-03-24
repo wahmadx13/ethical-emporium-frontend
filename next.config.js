3db663d6bfbad96abc5b1df77ef01343091c2c98
Waseem <wasim_ahmad2003@yahoo.com>
2024-03-24 16:48:04 +0500

    complete basic structure

--- next.config.js
+++ next.config.js
@@ -16,6 +16,21 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_BACKEND_BASE_URL: process.env.NEXT_BACKEND_BASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_AWS_REGION: process.env.NEXT_AWS_REGION,
+    NEXT_USER_POOL_ID: process.env.NEXT_USER_POOL_ID,
+    NEXT_URL_POOL_ARN: process.env.NEXT_URL_POOL_ARN,
+    NEXT_USER_POOL_CLIENT_ID: process.env.NEXT_USER_POOL_CLIENT_ID,
+    NEXT_USER_POOL_IDENTITY_POOL_ID:
+      process.env.NEXT_USER_POOL_IDENTITY_POOL_ID,
+    NEXT_USER_POOL_IDENTITY_POOL_ARN:
+      process.env.NEXT_USER_POOL_IDENTITY_POOL_ARN,
+    NEXT_REDIRECT_LOGNIN_URL: process.env.NEXT_REDIRECT_LOGNIN_URL,
+    NEXT_REDIRECT_LOGOUT_URL: process.env.NEXT_REDIRECT_LOGOUT_URL,
+    NEXT_TINYMCE_API_KEY: process.env.NEXT_TINYMCE_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
